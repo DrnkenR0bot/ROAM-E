de5a030c10feb057b0603d5271dfb32efc6b8586
dok <aporwitz@gmail.com>
2026-02-07 17:43:12 -0700

    add motor and servo code

--- CircuitPython code/code.py
+++ CircuitPython code/code.py
@@ -1,13 +1,14 @@
 import time
 import board
 import pwmio
-#import simpleio
-#import digitalio
 import adafruit_hcsr04
 from adafruit_motor import servo
 from note_frequencies import cMaj_scale
-#from motor_dr import Motor, Loco
+from motor_dr import Motor, Loco
 
+#
+# Component initialization
+#
 
 # Initialize piezo buzzer
 piezo = pwmio.PWMOut(board.GP22, variable_frequency=True)
@@ -15,20 +16,26 @@ piezo = pwmio.PWMOut(board.GP22, variable_frequency=True)
 # Initialize sonar
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP1, echo_pin=board.GP0)
 
-# Initialize servos
+# Initialize neck servos
 pa = pwmio.PWMOut(board.GP12, duty_cycle=2**15, frequency=100)
 pb = pwmio.PWMOut(board.GP13, duty_cycle=2**15, frequency=100)
 sa = servo.Servo(pa)
 sb = servo.Servo(pb)
 
+# Initialize left and right motors
+left_motor = Motor(board.GP10, board.GP11, down_calibrate=1.0)
+right_motor = Motor(board.GP8, board.GP9, down_calibrate=1.0)
+
+# Initialize Loco(motion) control
+loco = Loco(left_motor, right_motor)
+loco.stop()
 
 
+#
+# Function definitions
+#
 
-
-
-
-
-def distance(cm2xscale: float = 1.e-2):
+def distance(cm2xscale: float = 1.):
     """
     Return sonar distance to object.
     
@@ -59,14 +66,38 @@ def beep(buzzer, frequency, duration: float = 0.5, volume_exponent: int = 11):
     buzzer.duty_cycle = OFF
 
 
-
-
+#
+# Main
+#
 if __name__ == "__main__":
 
-    for key, value in cMaj_scale.items():
+    # Power up buzzer tones
+    for key, _ in cMaj_scale.items():
         if key[0] == "G":
             beep(piezo, cMaj_scale[key], duration=0.1)
 
+    # Servo test
+    sa.angle = 0
+    sb.angle = 0
+    time.sleep(0.5)
+    for angle in range(0, 180, 5):
+        sa.angle = angle
+        sb.angle = angle
+        time.sleep(0.2)
+    sa.angle = 90
+    sb.angle = 90
+
+    # Motor test
+    loco.forward(speed=0.5)
+    time.sleep(3)
+    loco.stop()
+    right_motor.forward()
+    time.sleep(1)
+    loco.stop()
+    left_motor.forward()
+    time.sleep(1)
+    loco.stop()
+
     while True:
-        print(statistical_distance(cm2xscale=1.))
+        print(statistical_distance())
         time.sleep(1)
